28ee4c8438728b9d1fe714fa63644f47af6ff292
XMo-afk <108943678+XMo-afk@users.noreply.github.com>
2024-07-09 13:07:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "happy birthday putry🎉✨🎊",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "ini adalah hari penting, semoga",  // 同上...
+        "memberikan banyak kebahagiaan buat putry",
+        "ku doa'in apapun keinginan putry",
+        "semoga secepatnya cepet terkabulkan",
+        "aminn",
+        "dan semoga putry diberikan kesehatan",
+        "panjang umur dan sehat selalu",
+        "udah nambah 1thn",
+        "harus lebih dewasa dalam ngadepin hal apapun",
+        "menjadi pribadi yg sabar dalam menghadapi masalah",
+        "harus lebih semangat lagii",
+        "udah sii kurang lebih itu aja",
+        "sekali lagi hbd putryy",
+        "YAMI~~",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai1.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "di tap putry",
+        play: "tapi lagi",
+        bannar_coming: "lagi",
+        balloons_flying: "lagi",
+        cake_fadein: "tap lagi",
+        light_candle: "lagi",
+        wish_message: "sekali lagi",
+        story: "A MESSAGE FOR YOU",
+    }
+};
